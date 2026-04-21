0525c229ea97f2aae6e9c513fc37bb8670ebb8af
rajayush01 <119657235+rajayush01@users.noreply.github.com>
2026-04-21 15:14:43 +0530

    small changes

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Menu, X, ChevronDown } from 'lucide-react';
 import { Link } from 'react-router-dom';
 import logo from '../../assets/tilaklogo1.png';
